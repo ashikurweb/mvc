1c29d34bff0d2bc7a3b3e74e432e8c38c33137ba
ashikurweb <ashikurrahman7194@gmail.com>
2025-07-25 11:59:58 +0600

    tailwind 4 installed

--- resources/views/home.php
+++ resources/views/home.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tailwind CSS Test</title>
+    <link rel="stylesheet" href="/css/tailwind.css">
 </head>
 <body>
-    <h1>Database Connection Test</h1>
-    <p><?= htmlspecialchars($dbStatus ?? 'No status') ?></p>
+    <h1 class="text-6xl mt-10 text-green-500 font-bold underline">Database Connection Test</h1>
+    <p class="text-lg text-red-500"><?= htmlspecialchars($dbStatus ?? 'No status') ?></p>
 </body>
 </html>
